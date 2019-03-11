c0c0256b04c8fb6fce27129626425a181d776c6c
wang <648551224@qq.com>
2019-03-11 19:14:25 +0800

    最新

--- main/views.py
+++ main/views.py
@@ -4,6 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def main(request):
-    return render(request, 'main/main.html')
-
-
+    return render(request, 'main/main')
--- subject/urls.py
+++ subject/urls.py
@@ -4,6 +4,6 @@ from subject import views
 
 # from main.views import main
 urlpatterns = [
-    path('', views.show_subject)
+    path("", views.show_subject)
 
 ]
